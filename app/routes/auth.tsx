f1504c4a8c0d96d8da0fb1525300abe2cd1f1719
Giovane Forlenza <giovane.franciscoforl@gmail.com>
2025-12-06 16:21:49 -0300

    Added routes and authentication checking

---
+++ app/routes/auth.tsx
@@ -0,0 +1,58 @@
+import { usePuterStore } from "lib/puter";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+
+export const meta = () => {
+  [
+    { title: "Resumind | Auth" },
+    { name: "description", content: "Log into your account" },
+  ];
+};
+
+const auth = () => {
+  const { isLoading, auth } = usePuterStore();
+  const location = useLocation();
+  const next = location.search.split("next=")[1];
+  const navigate = useNavigate();
+
+  //? Redirect to the page the user wants to go if authenticated
+  useEffect(() => {
+    if (auth.isAuthenticated) {
+      navigate(next);
+    }
+  }, [auth.isAuthenticated, next]);
+
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center">
+      <div className="gradient-border shadow-lg">
+        <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+          <div className="flex flex-col items-center gap-2 text-center">
+            <h1>Welcome</h1>
+            <h2>Log in to Continue Your Job Journey</h2>
+          </div>
+          <div className="">
+            {isLoading ? (
+              <button className="auth-button animate-pulse">
+                <p>Signing you in...</p>
+              </button>
+            ) : (
+              <>
+                {auth.isAuthenticated ? (
+                  <button className="auth-button" onClick={auth.signOut}>
+                    <p>Sign out</p>
+                  </button>
+                ) : (
+                  <button className="auth-button" onClick={auth.signIn}>
+                    <p>Sign in</p>
+                  </button>
+                )}
+              </>
+            )}
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default auth;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 import { resumes } from "../../constants/index";
 import ResumeCard from "~/components/ResumeCard";
+import { useLocation, useNavigate } from "react-router";
+import { useEffect } from "react";
+import { usePuterStore } from "lib/puter";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,6 +14,15 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { isLoading, auth } = usePuterStore();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  //? Redirect to the page the user wants to go if authenticated
+  useEffect(() => {
+    if (!auth.isAuthenticated) navigate("/auth?next=/");
+  }, [auth.isAuthenticated]);
+
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
